6959b7922e1ee70c4edac81975fdf1c0964fc304
mingu0429 <73528227+mingu0429@users.noreply.github.com>
2022-10-18 08:57:43 +0900

    [Bronze V] Title: 윤년, Time: 208 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a % 400 == 0){
+            System.out.print("1");
+        }else if((a % 4 == 0)&&(a % 100 != 0)){
+            System.out.print("1");
+        }else{
+            System.out.print("0");
+        }
+    }
+}
