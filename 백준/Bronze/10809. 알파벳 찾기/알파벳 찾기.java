565d0b68cce8fbc8117bcc9c1e796ef713d59bbb
mingu0429 <73528227+mingu0429@users.noreply.github.com>
2023-01-05 11:29:08 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 148 ms, Memory: 15884 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -6,6 +6,7 @@ public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));       
         String word = br.readLine();              
+        br.close();
         
         int[] arr = new int[26];
         for(int i = 0; i < arr.length; i++){
