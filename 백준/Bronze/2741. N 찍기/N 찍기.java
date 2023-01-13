e689b8d9071e3972d69cdb527c2b2e5c94ca5a89
mingu0429 <73528227+mingu0429@users.noreply.github.com>
2023-01-13 10:45:44 +0900

    [Bronze V] Title: N 찍기, Time: 284 ms, Memory: 21284 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -1,16 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.close();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        br.close();
         
         int num = 1;
         
         while(num <= n){
-            System.out.println(num);
+            bw.write(num + "\n");
             num++;
         }
+        bw.flush();
+        bw.close();
     }
 }
