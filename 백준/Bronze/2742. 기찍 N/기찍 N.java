e2683c79172098e9a6180cbe486b60c28ca0fb3d
mingu0429 <73528227+mingu0429@users.noreply.github.com>
2023-01-14 18:31:43 +0900

    [Bronze IV] Title: 기찍 N, Time: 292 ms, Memory: 21244 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int num = Integer.parseInt(br.readLine());
+        br.close();
+        
+        while(num > 0){
+            bw.write(num + "\n");
+            num--;
+        }
+        bw.flush();
+        bw.close();
+    }
+}
