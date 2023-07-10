78ea81a7f8a2d6614df502f1b1cf3dedf053357c
mingu0429 <73528227+mingu0429@users.noreply.github.com>
2023-07-10 12:58:38 +0900

    [unrated] Title: 더 크게 합치기, Time: 1.27 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        int ab = Integer.parseInt(String.valueOf(a) + String.valueOf(b));
+        int ba = Integer.parseInt(String.valueOf(b) + String.valueOf(a));
+        
+        if(ab >= ba){
+            answer = ab;
+        }else{
+            answer = ba;
+        }
+        
+        return answer;
+    }
+}
