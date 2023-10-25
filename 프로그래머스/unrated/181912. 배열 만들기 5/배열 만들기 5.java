5d5f21086679666543ec28939ce9505dc75895c2
mingu0429 <73528227+mingu0429@users.noreply.github.com>
2023-10-25 18:47:08 +0900

    [level unrated] Title: 배열 만들기 5, Time: 32.71 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.java
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] intStrs, int k, int s, int l) {
         
-        List<Integer> list = new ArrayList<Integer>();
+        List<Integer> list = new ArrayList<>();
         
         for(int i = 0; i < intStrs.length; i++){
             char[] charArr = intStrs[i].toCharArray();
