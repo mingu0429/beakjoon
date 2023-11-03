6a3f3fbf518c34e41be80ef67281392ce911322d
mingu0429 <73528227+mingu0429@users.noreply.github.com>
2023-11-03 15:10:16 +0900

    [level unrated] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.11 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        
+        int count = 0;
+        
+        for(int i = 0; i < myString.length(); i++){
+            if(myString.substring(i).startsWith(pat)){
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
