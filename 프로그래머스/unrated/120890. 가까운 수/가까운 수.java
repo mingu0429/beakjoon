fc5a9b71c228f6bd4febf0abac7afee875dbc80d
mingu0429 <73528227+mingu0429@users.noreply.github.com>
2023-11-22 09:21:24 +0900

    [level unrated] Title: 가까운 수, Time: 0.01 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120890. 가까운 수/가까운 수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] array, int n) {
+        int answer = array[0];
+        
+        for(int i = 1; i < array.length; i++){
+            if(Math.abs(array[i] - n) < Math.abs(answer - n)){
+                answer = array[i];
+            }else if(Math.abs(array[i] - n) == Math.abs(answer - n)){
+                answer = Math.min(array[i] ,answer);
+            }
+        }
+        return answer;
+    }
+}
