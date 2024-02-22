7dfb4f5edf0f8b9c3dda01352f06316f74313dfb
mingu0429 <73528227+mingu0429@users.noreply.github.com>
2024-02-22 20:49:43 +0900

    [level 1] Title: 소수 찾기, Time: 201.23 ms, Memory: 52 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i = 2; i <= n; i++){
+            answer += isPrime(i);
+        }
+        return answer;
+    }
+    
+    public int isPrime(int n){
+        for(int i = 2; i <= Math.sqrt(n); i++){
+            if(n % i == 0){
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
