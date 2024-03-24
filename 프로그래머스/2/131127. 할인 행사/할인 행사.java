5148c441125c305e592dbdc57ff0b4b9b222c3e8
mingu0429 <73528227+mingu0429@users.noreply.github.com>
2024-03-24 23:46:56 +0900

    [level 2] Title: 할인 행사, Time: 0.03 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        
+        for(int i = 0; i < discount.length - 9; i++){
+            int j;
+            for(j = 0; j < want.length; j++){
+                int temp = 0;
+                for(int k = i; k < i + 10; k++){
+                    if(discount[k].equals(want[j])) temp++;
+                }
+                if(temp < number[j]) break;
+            }
+            if(j == want.length) answer++;
+        }
+        return answer;
+    }
+}
